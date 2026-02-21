538407963d24e94cc4933b9de01dbebfbe0dfe5a
Gokulraj_C <cagokulraj1410@gmail.com>
2026-02-21 09:50:24 +0530

    Delete census directory

--- census/src/main/java/com/kce/census/Repository/ApplicationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kce.census.Repository;
-
-import com.kce.census.Entity.Application;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ApplicationRepository extends MongoRepository<Application, String> {
-
-}
